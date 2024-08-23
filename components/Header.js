259be658deb58a55c15c4a8a15e9658e9dd412c0
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-23 07:23:37 +0300

    router update

--- components/Header.js
+++ components/Header.js
@@ -8,10 +8,13 @@ const Header = () => {
         <a href="#">Tiara Petsits</a>
         {/* nav */}
         <nav className="text-xl flex gap-x-4 lg:gap-x-12   md:mr-64">
+          <a href="#" className="hidden md:block">
+            About
+          </a>
           <a href="#">Services</a>
-          <a href="#">Gallery</a>
-          <a href="#">Reviews</a>
-          <a href="#">Contact</a>
+          <a href="/gallery">Gallery</a>
+          <a href="/reviews">Reviews</a>
+          <a href="/contact">Contact</a>
         </nav>
       </div>
     </header>
