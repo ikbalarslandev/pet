7b3a6d1945d5fccc1698500a06a72f1b45096e36
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-26 09:41:37 +0300

    photos updated bug fix

--- components/Pets.js
+++ components/Pets.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Pet1Img from "../public/img/pets/1.jpg";
 import Pet2Img from "../public/img/pets/2.jpg";
-// import Pet3Img from "../public/img/pets/3.png";
-// import Pet4Img from "../public/img/pets/4.png";
-// import Pet5Img from "../public/img/pets/5.png";
-// import Pet6Img from "../public/img/pets/6.png";
-// import Pet7Img from "../public/img/pets/7.png";
-// import Pet8Img from "../public/img/pets/8.png";
+import Pet3Img from "../public/img/pets/3.jpg";
+import Pet4Img from "../public/img/pets/4.jpg";
+import Pet5Img from "../public/img/pets/5.jpg";
+import Pet6Img from "../public/img/pets/6.jpg";
+import Pet7Img from "../public/img/pets/7.jpg";
+import Pet8Img from "../public/img/pets/8.jpg";
 
 const pets = [
   {
@@ -20,36 +20,36 @@ const pets = [
     category: "Daphne",
     image: Pet2Img,
   },
-  // {
-  //   id: 3,
-  //   category: "Dolma",
-  //   image: Pet3Img,
-  // },
-  // {
-  //   id: 4,
-  //   category: "Fifi",
-  //   image: Pet4Img,
-  // },
-  // {
-  //   id: 5,
-  //   category: "Tobey",
-  //   image: Pet5Img,
-  // },
-  // {
-  //   id: 6,
-  //   category: "Milky",
-  //   image: Pet6Img,
-  // },
-  // {
-  //   id: 7,
-  //   category: "Soda",
-  //   image: Pet7Img,
-  // },
-  // {
-  //   id: 8,
-  //   category: "Bean",
-  //   image: Pet8Img,
-  // },
+  {
+    id: 3,
+    category: "Dolma",
+    image: Pet3Img,
+  },
+  {
+    id: 4,
+    category: "Fifi",
+    image: Pet4Img,
+  },
+  {
+    id: 5,
+    category: "Tobey",
+    image: Pet5Img,
+  },
+  {
+    id: 6,
+    category: "Milky",
+    image: Pet6Img,
+  },
+  {
+    id: 7,
+    category: "Soda",
+    image: Pet7Img,
+  },
+  {
+    id: 8,
+    category: "Bean",
+    image: Pet8Img,
+  },
 ];
 
 const Pets = () => {
