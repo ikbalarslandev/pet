ef2c3aefcd6d1f085c2b58163718491a64adc133
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-27 09:09:59 +0300

    image fix

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,18 +1,18 @@
 import GallerySwiper from "../components/gallerySwiper";
 
 const fifiImages = [
-  "/img/gallery/fifi/1fifi.png",
-  "/img/gallery/fifi/2fifi.png",
-  "/img/gallery/fifi/3fifi.png",
-  "/img/gallery/fifi/4fifi.png",
-  "/img/gallery/fifi/5fifi.png",
-  "/img/gallery/fifi/6fifi.jpg",
+  "/img/gallery/fifi/1.jpg",
+  "/img/gallery/fifi/2.jpg",
+  "/img/gallery/fifi/3.jpg",
+  "/img/gallery/fifi/4.jpg",
+  "/img/gallery/fifi/5.jpg",
+  "/img/gallery/fifi/6.jpg",
 ];
 
 const Gallery = () => {
   return (
-    <div className="text-center bg-cyan-500 h-[100vh]">
-      <div className="py-28">
+    <div className="text-center bg-cyan-500 h-[100vh] ">
+      <div className="pt-16">
         <h1 className="font-bold text-3xl">Fifi</h1>
         <GallerySwiper images={fifiImages} />
       </div>
